417d941a43b5391333d3a03331dcd1397b057d97
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-22 10:39:17 +0800

    Add files via upload

---
+++ day1/Hello5.py
@@ -0,0 +1,12 @@
+# 變數的應用
+chinese = 100
+math = 77
+english = 82
+print(chinese, math, english)  # 將所有變數內容印出
+# 求總分與平均
+total = chinese + math + english
+print("總分:", total)
+average = total / 3
+print("平均:", average)
+average = total // 3
+print("平均:", average)
