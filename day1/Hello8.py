2f7caf0554987a8fcfac1dd1ace017397d338e54
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-22 12:05:47 +0800

    Update Hello8.py

--- day1/Hello8.py
+++ day1/Hello8.py
@@ -1,7 +1,7 @@
 # 整合應用-找零錢程式
 # 買飲料一瓶 123 元, 付了 1000 元, 請問要找幾元 ? 如何找 ?
-price = 123
-pay = 1000
+price = int(input('請輸入飲料價格:'))
+pay = int(input('請輸入付款金額:'))
 exchange = pay - price  # 找錢
 print("飲料:", price, "付款:", pay, "應找:", exchange)
 # 計算找零的面額與數量
