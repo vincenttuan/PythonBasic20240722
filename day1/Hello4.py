f29ec8509cb84a4b015ab18973d85f8370e565cb
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-22 11:22:45 +0800

    Update Hello4.py

--- day1/Hello4.py
+++ day1/Hello4.py
@@ -1,1 +1,11 @@
 # 今有雞、兔同籠，上有三十五頭，下九十四足。問雉、兔各幾何？
+total = 35  # 三十五頭
+feet = 94  # 九十四足
+
+rabbit = (feet - (total*2)) / (4-2)
+chicken = total - rabbit
+
+print("雞: %d 隻, 兔: %d 隻" % (chicken, rabbit))
+
+
+
