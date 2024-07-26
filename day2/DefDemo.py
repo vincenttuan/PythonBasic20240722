00d2299a2b4e71af79d0ed02f21828c7fbec0d46
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-26 11:50:50 +0800

    Add files via upload

---
+++ day2/DefDemo.py
@@ -0,0 +1,12 @@
+# 自訂函式
+# 印出訊息的自訂函示
+def print_message(name):  # name 用來接收參數
+    print("訊息: %s 生日快樂" % name)
+    print("Message: %s Happy Birthday" % name)
+
+
+# 使用自訂訊息
+print_message('John')  # 字串圈住符號可用 ' 或 "
+print_message("Mary")
+print_message("Jack")
+print_message('Rose')
