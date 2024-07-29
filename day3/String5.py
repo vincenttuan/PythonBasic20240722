7b37bd9cedd87f2968127dc48cc72f3df5902f5f
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-29 10:50:13 +0800

    Add files via upload

---
+++ day3/String5.py
@@ -0,0 +1,22 @@
+
+def calcBmi(index):
+    h = float(heights[index])
+    w = float(weights[index])
+    bmi = w / (h / 100) ** 2
+    print(h, w, bmi)
+
+
+# 字串分組練習
+# int("字串") 字串轉整數
+# float("字串") 字串轉浮 點數
+data1 = "170,160.5,180"  # 身高cm
+data2 = "60,45,89.5"  # 體重kg
+# 請計算出每組 bmi=?
+heights = data1.split(",")
+weights = data2.split(",")
+print(heights, weights)
+#--------------------------------
+calcBmi(0)
+calcBmi(1)
+calcBmi(2)
+
