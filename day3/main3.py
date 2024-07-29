fd94e443c5a5eeb73490b53e46d7efe5c616e49b
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-29 11:44:42 +0800

    Add files via upload

---
+++ day3/main3.py
@@ -0,0 +1,8 @@
+# 循環
+import random
+
+if __name__ == '__main__':
+    # 不斷印出 1~100 的隨機數
+    while True:
+        n = random.randint(1, 100)
+        print(n)
---
+++ day3/main4.py
@@ -0,0 +1,8 @@
+# 利用循環寫出8星彩電腦選號
+import random
+
+if __name__ == '__main__':
+    count = 8
+    while count > 0:
+        print(count)
+        count = count - 1
