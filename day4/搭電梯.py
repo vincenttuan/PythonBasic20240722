898d4d9c12cf1f55072c8404c8d678fef8914dfa
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-31 09:52:12 +0800

    Add files via upload

--- day4/搭電梯.py
+++ day4/搭電梯.py
@@ -3,12 +3,16 @@ import time
 
 print('本大樓有1~7層')
 target_floor = int(input('請輸入樓層:'))
-for floor in range(1, 8):
-    print('電梯上樓', floor)
-    time.sleep(1)  # 停一秒
-    if floor == target_floor:
-        print('電梯到達指定樓層, 請出電梯')
-        break  # for 迴圈結束
+# 防呆
+if 1 <= target_floor <= 7:
+    for floor in range(1, 8):
+        print('電梯上樓', floor)
+        time.sleep(1)  # 停一秒
+        if floor == target_floor:
+            print('電梯到達指定樓層, 請出電梯')
+            break  # for 迴圈結束
+else:
+    print('樓層輸入不正確')
 
 print("程式結束!")
 
