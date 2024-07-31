675974520e4b21a56417ba3007128f572cba60c2
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-31 09:46:56 +0800

    Add files via upload

---
+++ day4/搭電梯.py
@@ -0,0 +1,15 @@
+# 利用 for 模擬搭電梯
+import time
+
+print('本大樓有1~7層')
+target_floor = int(input('請輸入樓層:'))
+for floor in range(1, 8):
+    print('電梯上樓', floor)
+    time.sleep(1)  # 停一秒
+    if floor == target_floor:
+        print('電梯到達指定樓層, 請出電梯')
+        break  # for 迴圈結束
+
+print("程式結束!")
+
+
