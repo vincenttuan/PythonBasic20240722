f0bafba8cbb97d0dcf5a47e7375b65a1e92c26f4
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-02 09:46:34 +0800

    Update Algorithm.py

--- day5/Algorithm.py
+++ day5/Algorithm.py
@@ -4,8 +4,7 @@ if __name__ == '__main__':
     price = 10  # 飲料價格
     amount = 5  # 購買數量
     x, y = 2, 1  # 買 x 送 y
-    print("飲料 %d 元 買 %d 送 %d 買進 %d 瓶須支付多少錢?" %
-          (price, x, y, amount))
+    print("飲料 %d 元 買 %d 送 %d 買進 %d 瓶須支付多少錢?" % (price, x, y, amount))
     # 試問應付多少錢 ?
     # 根據 x, y 先算出要付幾瓶的錢
     bottles = amount // (x+y) * x + amount % (x+y)
