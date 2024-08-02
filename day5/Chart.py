e12d9d30083e7d153712877f6e90951eb77d97ff
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-02 11:52:42 +0800

    Update Chart.py

--- day5/Chart.py
+++ day5/Chart.py
@@ -11,7 +11,16 @@ if __name__ == '__main__':
     print(scores)
 
     # 繪製折線圖
-    plt.plot(names, scores)
+    #plt.plot(names, scores, '*--', color='red')
+    # 繪製長條圖
+    #plt.bar(names, scores, color='blue')
+    # 繪製圓餅圖
+    plt.pie(scores, labels=names, autopct='%.1f%%', startangle=140)
+
+    # 設定圖標資訊
+    plt.xlabel('student name')
+    plt.ylabel('score')
+    plt.title('student score chart')
     # 顯示圖型
     plt.show()
 
