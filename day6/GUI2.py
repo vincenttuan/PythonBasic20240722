fea0c3b276c09919daeba046dd549b5b833df202
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-05 11:02:12 +0800

    Add files via upload

--- day6/GUI2.py
+++ day6/GUI2.py
@@ -10,7 +10,14 @@ import tkinter
 if __name__ == '__main__':
     win = tkinter.Tk()
 
-    hello_label = tkinter.Label(win, text="Hello")
+    hello_label = tkinter.Label(win,
+                                text="Hello",  # 標籤文字
+                                bg='green',    # 背景顏色
+                                fg='yellow',   # 前景顏色(字體顏色)
+                                font=('Arial', 20),  # 字型與大小
+                                width=15,  # 標籤寬度
+                                height=2   # 標籤高度
+                                )
     hello_label.pack()  # 將 hello_label 配置在 win 中
 
     b1 = tkinter.Button(win, text="Prev")
