40fef35dba10e60c350594f7e4331880a8bacf2b
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-07 09:46:23 +0800

    Add files via upload

--- day7/GUI_Grid.py
+++ day7/GUI_Grid.py
@@ -16,12 +16,16 @@ btn2 = tkinter.Button(text='Btn2', font=myfont)
 btn3 = tkinter.Button(text='Btn3', font=myfont)
 btn4 = tkinter.Button(text='Btn4', font=myfont)
 btn5 = tkinter.Button(text='Btn5', font=myfont)
+btn6 = tkinter.Button(text='Btn6', font=myfont)
+btn7 = tkinter.Button(text='Btn7', font=myfont)
 
 btn1.grid(row=0, column=0, sticky='EWNS')   # EWNS: 無縫隙元件填滿
 btn2.grid(row=0, column=1, columnspan=2, sticky='EWNS')
-btn3.grid(row=1, column=0, sticky='EWNS')
+btn3.grid(row=1, column=0, rowspan=2, sticky='EWNS')
 btn4.grid(row=1, column=1, sticky='EWNS')
 btn5.grid(row=1, column=2, sticky='EWNS')
+btn6.grid(row=2, column=1, sticky='EWNS')
+btn7.grid(row=2, column=2, sticky='EWNS')
 
 win.mainloop()
 
