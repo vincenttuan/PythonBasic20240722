8ac32d24f7096e1aa79edc0d574877a12d409b6d
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-07 09:56:22 +0800

    Add files via upload

---
+++ day7/GUI_BMI.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import font
+
+'''
+BMI 計算機
++--------+---------+
+| Height |  170.0  |  <-- Label, Entry
++--------+---------+
+| Weight |   60.5  |  <-- Label, Entry
++--------+---------+
+|  Reset |  Submit |  <-- Button, Button
++--------+---------+
+|      20.76       |  <-- Label, Label
++--------+---------+
+'''
