6f10601863310351080b5fad6d231d9df7e547cc
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-07 10:12:16 +0800

    Update GUI_BMI.py

--- day7/GUI_BMI.py
+++ day7/GUI_BMI.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     win.title('BMI 計算機')
     win.geometry("600x250")
     # 建立 GUI 元件
-    myfont = font.Font(family='Arial', size=36, weight='bold')
+    myfont = font.Font(family='Arial', size=30, weight='bold')
     height_label = tkinter.Label(text='身高', font=myfont)
     height_entry = tkinter.Entry(font=myfont, justify=tkinter.RIGHT)
     weight_label = tkinter.Label(text='體重', font=myfont)
@@ -35,6 +35,15 @@ if __name__ == '__main__':
     result_label = tkinter.Label(text='0.00', font=myfont)
     # ------------------------------------------------------------------
     # 布局 GUI 元件
+    height_label.grid(row=0, column=0, sticky='EWNS')
+    height_entry.grid(row=0, column=1, sticky='EWNS')
+    weight_label.grid(row=1, column=0, sticky='EWNS')
+    weight_entry.grid(row=1, column=1, sticky='EWNS')
+    reset_btn.grid(row=2, column=0, sticky='EWNS')
+    submit_btn.grid(row=2, column=1, sticky='EWNS')
+    result_label.grid(row=3, column=0, columnspan=2, sticky='EWNS')
+    # ------------------------------------------------------------------
+    win.mainloop()
 
 
 
