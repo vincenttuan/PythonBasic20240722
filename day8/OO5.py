a79a780f4246341dce1bd49aa8c86fa7b6606ea6
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-09 11:41:57 +0800

    Add files via upload

---
+++ day8/OO5.py
@@ -0,0 +1,25 @@
+# 飲料訂單系統
+from day8.OO4 import get_menu
+
+if __name__ == '__main__':
+    menu = get_menu()
+    print('Menu:')
+    for index, beverage in enumerate(menu):
+        print(index, beverage)
+    # -------------------------------------
+    print("-----------------")
+    # 訂單
+    order = []
+    while True:
+        data = input('請選擇商品代號(0~11), 離開請輸入(q) ==> ')
+        if data == 'q':
+            break
+        index = int(data)  # 將 data 轉 int, 得到使用者所購買的商品代號
+        # 列印所購買的飲料
+        print(menu[index])
+        # 加入訂單
+        order.append(menu[index])
+    # -------------------------------------------------------
+    print("\n訂單:")
+    for beverage in order:
+        print(beverage)
