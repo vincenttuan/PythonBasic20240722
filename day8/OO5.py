71841de52bdf675f4cd779611e09af428a310a34
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-09 11:51:46 +0800

    Update OO5.py

--- day8/OO5.py
+++ day8/OO5.py
@@ -15,6 +15,10 @@ if __name__ == '__main__':
         if data == 'q':
             break
         index = int(data)  # 將 data 轉 int, 得到使用者所購買的商品代號
+        # 判斷 index 範圍
+        if index < 0 or index >= len(menu):
+            print('飲料號碼超過範圍')
+            break
         # 列印所購買的飲料
         print(menu[index])
         # 加入訂單
@@ -23,3 +27,6 @@ if __name__ == '__main__':
     print("\n訂單:")
     for beverage in order:
         print(beverage)
+    # 計算此筆訂單總金額
+    total_sum = sum(beverage.price for beverage in order)
+    print("訂單總金額: %d" % total_sum)
