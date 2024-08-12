6cf95dee9c94ec481305e2839fe7841335e4332a
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-12 12:12:16 +0800

    Add files via upload

--- day9/qrcode_generate.py
+++ day9/qrcode_generate.py
@@ -1,10 +1,13 @@
 # QRCode 產生器
 # 請先安裝 qrcode 套件
 import qrcode
+import base64  # 編碼
 
 # 建立 qrcode 碼
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
-qr.add_data("可樂 1 瓶 35 元")
+data = "可樂 1 瓶 35 元"
+#qr.add_data(data)  # 明碼
+qr.add_data(base64.b64encode(data.encode('utf-8')).decode('utf-8'))  # 編碼
 qr.make(fit=True)
 # 建立 qrcode 圖檔
 img = qr.make_image(fill_color="black", back_color="white")
