7cc0b7d80ca4772cc9f66c6d0565e7cbf74cd40a
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-14 09:49:37 +0800

    Update 人臉照片偵測.py

--- day10/人臉照片偵測.py
+++ day10/人臉照片偵測.py
@@ -6,6 +6,15 @@ face_cascade = cv2.CascadeClassifier('ai/xml/haarcascade_frontalface_default.xml
 # 讀取影像檔
 frame = cv2.imread('ai/image/girl.jpg')
 
+# 印出圖片原始資料
 print(frame)
+
+# 顯示圖片
+cv2.imshow('My Image', frame)
+
+# 在圖片上按下任意鍵即可離開程式
+c = cv2.waitKey(0)
+
+
 
 
